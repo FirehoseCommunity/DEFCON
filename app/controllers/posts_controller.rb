8cbecd4154506ed87ca9342254b5964b91d5d207
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-12-09 01:33:26 +0000

    Add index, new, creeate, edit, update, and destroy actions to to posts_controller Add user authentication before_action to posts_controller Add modal for post creation to index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,33 @@
 class PostsController < ApplicationController
+  # before_action :authenticate_user!
+
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = @user.post.create(post_params)
+    if @post.valid?
+      redirect_to posts_path
+    else
+      render :new, :status => :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,4 +4,10 @@ class PostsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "require user sign_in before viewing posts" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
+
 end
