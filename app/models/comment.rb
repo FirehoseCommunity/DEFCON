0d192a03767d938b64ddc9bc15d9aa3b2e2eec5f
Cecelia <ceceliahavens@hotmail.com>
2015-12-13 22:21:31 +0000

    setup beginning of TDD for Comments on posts, lord help me it's a little sloppy still, TDD is not my friend....yet...
    Also changed the model. user.rb test of another coder to reflect true instead of false or nil --CD

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  def comment_on_post
+    self.user_id
+  end 
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "comment_on_post" do 
+     comment = Comment.create(:user_id => 1, :message => 'Tacocats and Stackcats')
+    expected = 1  
+     actual = comment.comment_on_post
+  # comment = FactoryGirl.create(:comment, :author => 'Cecelia')
+  #   expected = 'KM#' + quote.id.to_s
+  #   actual = quote.unique_tag
+    assert_equal expected, actual
+    
+  end
+
 end
