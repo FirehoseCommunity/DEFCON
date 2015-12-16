b4acb2fcd290dc593265bcc5f78ce225d6894ede
Matt-Lepley <lepley.matt@gmail.com>
2015-12-16 14:22:10 +0000

    more personalized message.

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,8 +2,9 @@ class AppMailer < ActionMailer::Base
  
   default from: 'noreply@firehose-defcon.com'
  
-  def welcome_email
-    mail(to: 'matt@thefirehoseproject.com', 
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, 
          subject: "Welcome!")
   end
 end
