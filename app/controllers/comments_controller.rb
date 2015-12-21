7576bc781d132a1bd9b95127f86b19fc2bb83fda
Cecelia <ceceliahavens@hotmail.com>
2015-12-21 20:51:35 +0000

    trying to build the delete features

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,31 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+
 def create
   @post = Post.find(params[:post_id])
   @post.comments.create(comment_params.merge(:user => current_user))
   redirect_to post_path(@post)
 end
 
+def destroy
+  @post = comment.post
+  comment.destroy 
+  redirect_to post_path(@post)
+end
+
 private
+
+def require_comment
+  render_not_found unless @comment
+end
+
+def comment
+  @comment ||= Comment.find_by_id(params[:id])
+end
+
+def require_comment_destroyable
+    render_not_found(:forbidden) unless comment.controlled_by?(current_user)
+end
 
   def comment_params
     params.require(:comment).permit(:message)
