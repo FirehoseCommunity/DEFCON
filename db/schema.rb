6493ecf445517cf4472a74d0dc04f1a6863e421a
amarkpark <nospam@amarkpark.com>
2016-01-03 20:49:41 +0000

    Removed first-user-admin thing, added admin promotions, re-added default false admin value for users. ~AMP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160101022641) do
+ActiveRecord::Schema.define(version: 20160103070107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20160101022641) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
     t.boolean  "post_notification",      default: true
     t.boolean  "comment_notification",   default: true
     t.string   "name",                   default: "I Am Awesome"
