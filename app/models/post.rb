7dd3c4b40e4522b4622db42f7a7ca858fff7afbe
Jeff Gerlach <jeffgerlach06@gmail.com>
2016-01-03 17:14:28 -0700

    First try at url parsing methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
+  def extract_first_url_from_body
+    url_list = extract_first_url_from_string(body)
+    return url_list[0] if url_list.present? 
+  end
+
+  private
+    def extract_urls_from_string(input)
+      # borrowed from https://github.com/matthewrudy/regexpert/blob/master/lib/regexpert.rb
+      url_regex = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+      input.scan(url_regex)[0]
+    end
 end
