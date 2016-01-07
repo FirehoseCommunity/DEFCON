a8836ac33c7ecd9b59118efa110e069c04f451f9
amarkpark <nospam@amarkpark.com>
2016-01-07 22:58:35 +0000

    Very preliminary admin menu partial and admin namespace. ~AMP

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,5 @@
+class Admin::UsersController < AdminController
+  def update
+  end
+
+end
---
+++ test/controllers/admin/users_controller_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class Admin::UsersControllerTest < ActionController::TestCase
+  test "will allow admin to view admin pages" do
+    admin = FactoryGirl.create(:user, admin: true)
+    asser admin.admin?
+    sign_in admin
+    get :index
+    assert_redirected_to admin_users_path
+  end
+
+  test "will not allow non-admin to view admin pages" do
+    user = FactoryGirl.create(:user)
+    refute user.admin?
+    sign_in user
+    get :index
+    assert_redirected_to root_path
+  end
+
+  test "will allow admin to promote user" do
+    user = FactoryGirl.create(:user)
+    refute user.admin?
+    admin = FactoryGirl.create(:user, admin: true)
+    sign_in admin
+    put :update, id: user.id, user: { admin: true }
+    user.reload
+    assert user.admin?
+  end
+
+  test "will not allow non-admin to promote user" do
+    user = FactoryGirl.create(:user)
+    refute user.admin?
+    sign_in user
+    put :update, id: user.id, user: { admin: true }
+    refute user.admin?
+    assert_redirected_to root_path
+  end
+end
