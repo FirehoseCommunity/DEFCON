edb14df5b9156e3277cae83bc35d3fdb4e709804
Cecelia <ceceliahavens@hotmail.com>
2016-01-07 21:24:01 +0000

    adding  ActionMailer::Base.delivery_method = :test to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,6 @@ Defcon::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  ActionMailer::Base.delivery_method = :test
+ 
 end
