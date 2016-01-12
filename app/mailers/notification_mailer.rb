0a0e5991b1e5acded70b0e1209a5bdf8f101d04c
Nicola Guidi <nicola.guidi@me.com>
2016-01-12 04:37:22 +0100

    hook NotificationMailer and posts creation with a callback

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,9 @@
 class NotificationMailer < ActionMailer::Base
-  default to: Proc.new { User.pluck(:email) },
-    from: "noreply@firehose-defcon.com"
- 
+  default to: Proc.new { User.where(post_notification: true).pluck(:email) },
+          from: "noreply@firehose-defcon.com"
+
   def send_notification(user)
-   @user = user
+    @user = user
     mail(subject: "A new post from #{@user.email} has been added to the Defcon Student Group")
-    
   end
- 
 end
