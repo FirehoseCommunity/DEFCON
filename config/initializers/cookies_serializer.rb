bb27ac991acb7a9037aa5f1479b1b429097cca3f
Jeff Gerlach <jeffgerlach06@gmail.com>
2016-01-24 21:17:10 -0700

    Changing values back to defaults

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Defcon::Application.config.action_dispatch.cookies_serializer = :json
+Rails.application.config.action_dispatch.cookies_serializer = :json
