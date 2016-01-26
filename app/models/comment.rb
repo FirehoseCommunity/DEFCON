510432622d2840e833f44342b22b87398b2f3ea6
Jeff Gerlach <jeffgerlach06@gmail.com>
2016-01-25 21:59:46 -0700

    Add new field to user to track if they want to be notified of comments on posts they commented on. Update user dashboard to show this option and allow it to be updated. Create files for comment notification mailer.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,21 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  after_create :send_post_owner_notification, :send_post_interactor_notification
   
   def controlled_by?(user)
     self.user == user
-  end 
+  end
+
+  def send_post_owner_notification
+  end
+
+  def send_post_interactor_notification
+    @recipients = User.where("post_interaction_notification = ? AND email != ?", true, self.post.user.email)
+    unless @recipients.empty?
+      @recipients.each do |recipient|
+        NotificationMailer.send_notification(self.id, recipient.id).deliver_later
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114211510) do
+ActiveRecord::Schema.define(version: 20160126044555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,22 +42,22 @@ ActiveRecord::Schema.define(version: 20160114211510) do
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",             null: false
-    t.string   "encrypted_password",     limit: 255, default: "",             null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                         limit: 255, default: "",             null: false
+    t.string   "encrypted_password",            limit: 255, default: "",             null: false
+    t.string   "reset_password_token",          limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,              null: false
+    t.integer  "sign_in_count",                             default: 0,              null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                                                  null: false
-    t.datetime "updated_at",                                                  null: false
-    t.boolean  "admin",                              default: false
-    t.boolean  "post_notification",                  default: true
-    t.boolean  "comment_notification",               default: true
-    t.string   "name",                   limit: 255, default: "I Am Awesome"
+    t.datetime "created_at",                                                         null: false
+    t.datetime "updated_at",                                                         null: false
+    t.boolean  "admin",                                     default: false
+    t.boolean  "post_notification",                         default: true
+    t.boolean  "comment_notification",                      default: true
+    t.string   "name",                          limit: 255, default: "I Am Awesome"
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
@@ -65,7 +65,8 @@ ActiveRecord::Schema.define(version: 20160114211510) do
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
-    t.integer  "invitations_count",                  default: 0
+    t.integer  "invitations_count",                         default: 0
+    t.boolean  "post_interaction_notification",             default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
@@ -73,16 +74,5 @@ ActiveRecord::Schema.define(version: 20160114211510) do
   add_index "users", ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "votes", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "votable_id"
-    t.string   "votable_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
-  add_index "votes", ["votable_id", "votable_type"], name: "index_votes_on_votable_id_and_votable_type", using: :btree
 
 end
