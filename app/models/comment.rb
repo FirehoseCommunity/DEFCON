316aa1085385b7ee4eed4f6641b797a49b9f4150
Jeff Gerlach <jeffgerlach06@gmail.com>
2016-01-25 22:19:32 -0700

    Add comment mailer and associated views for email notifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,13 +8,17 @@ class Comment < ActiveRecord::Base
   end
 
   def send_post_owner_notification
+    if self.post.user.comment_notification?
+      CommentNotificationMailer.send_notification(self.post.id, self.id, self.post.user.id).deliver_later
+    end
   end
 
   def send_post_interactor_notification
+    # excludes post creator so they don't get two notifications
     @recipients = User.where("post_interaction_notification = ? AND email != ?", true, self.post.user.email)
     unless @recipients.empty?
       @recipients.each do |recipient|
-        NotificationMailer.send_notification(self.id, recipient.id).deliver_later
+        CommentNotificationMailer.send_notification(self.post.id, self.id, recipient.id).deliver_later
       end
     end
   end
