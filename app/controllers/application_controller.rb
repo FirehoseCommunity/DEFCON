c8174681e6ec6f10f20fd75f9b87e111b4ca2174
DarthDARKFather <artistsketchs@aol.com>
2016-03-08 22:35:57 +0000

    removed all the devise configuration sanitizer code from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_filter :configure_sanitized_params, if: :devise_controller?
-
-  #def configure_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:github_handle)}
-  #end
 
 
   def render_not_found(status = :not_found)
