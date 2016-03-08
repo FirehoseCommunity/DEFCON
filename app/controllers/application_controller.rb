e8ddbf2a7e85c22c5c072b4c44c85861abf1408a
DarthDARKFather <artistsketchs@aol.com>
2016-03-08 22:26:12 +0000

    is this what you wanted, what's commented out to be removed?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :configure_sanitized_params, if: :devise_controller?
+  #before_filter :configure_sanitized_params, if: :devise_controller?
 
-  def configure_sanitized_params
+  #def configure_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:github_handle)}
-  end
+  #end
 
 
   def render_not_found(status = :not_found)
