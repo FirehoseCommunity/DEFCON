fb9a149faf9730a2054f6c2d6d48af10d79cbf94
Robert Sapunarich <robertsapunarich@gmail.com>
2016-03-08 17:53:23 -0800

    Add callback URL to devise initializer. Make User model `omniauthable` and declare github omniauth provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         :omniauth_providers => [:github]
 
 
   has_many :posts, dependent: :destroy 
