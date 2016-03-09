7859241f48274b4f3ede9f4f162d33e5811c2ede
Robert Sapunarich <robertsapunarich@gmail.com>
2016-03-08 18:14:21 -0800

    Remove `repos` scope from github provider.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -283,7 +283,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, 'client_id', 'client_secret', scope: 'user,public_repo', callback_url: 'http://localhost:3000/posts'
+  config.omniauth :github, 'client_id', 'client_secret', scope: 'user', callback_url: 'http://localhost:3000/posts'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
